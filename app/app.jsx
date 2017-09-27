d8b1d2be59d3add0e75f87d8e2f86cb90d669445
JairoHernandez <jairomh@hotmail.com>
2017-09-27 15:21:36 -0500

    Add redux store

--- app/app.jsx
+++ app/app.jsx
@@ -4,6 +4,19 @@ var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
 var TodoApp = require('TodoApp');
 
+var actions = require('actions'); // alias in webpack.config.js
+var store = require('configureStore').configure();
+
+// listens to changes on my store
+store.subscribe(() => {
+  console.log('New state', store.getState());
+});
+
+// Chrome console output.
+store.dispatch(actions.addTodo('Clean the yard')); // Placed into todos: [{}] array of objects.
+store.dispatch(actions.setSearchText('yard')); // Placed into key 'searchText'.
+store.dispatch(actions.toggleShowCompleted()); // ""
+
 // Load Foundation
 $(document).foundation();
 
